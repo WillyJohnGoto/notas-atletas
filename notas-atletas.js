82f2eb82b89e86921cc220939dd98091082ca086
Willy John Nakamura Goto <willy.john95@gmail.com>
2024-12-04 18:38:47 -0300

    Create notas-atletas.js

---
+++ notas-atletas.js
@@ -0,0 +1,26 @@
+function retornarMedia(atletas){
+    console.log(atletas[0].notas.sort((a,b) => a-b));
+    return atletas[0].nome;
+}
+
+
+let atletas = [
+    {
+      nome: "Cesar Abascal",
+      notas: [10, 9.34, 8.42, 10, 7.88]
+    },
+    {
+      nome: "Fernando Puntel",
+      notas:  [8, 10, 10, 7, 9.33]
+    },
+    {
+      nome: "Daiane Jelinsky",
+      notas: [7, 10, 9.5, 9.5, 8]
+    },
+    {
+      nome: "Bruno Castro",
+      notas: [10, 10, 10, 9, 9.5]
+    }
+   ];
+
+   retornarMedia(atletas);
